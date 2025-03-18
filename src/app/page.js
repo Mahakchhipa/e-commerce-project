cb0f61e9939ece9f6f2bed1197916578d5b3aee2
Mahakchhipa <mehekjajpuraom700@gmail.com>
2025-03-18 18:40:52 +0530

    Project setup

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="text-center font-bold text-red-500 py-8 text-5xl ">
+    shree Ganesh 
+   </div>
+  );
+}
