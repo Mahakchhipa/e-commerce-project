11eb59a99dce9d763904c0e0088f5701bc1549c9
Mahakchhipa <mehekjajpuraom700@gmail.com>
2025-03-19 12:29:09 +0530

    Header setup

---
+++ src/app/_components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      this is my hero sections 
+    </div>
+  )
+}
+
+export default Hero
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div className="text-center font-bold text-red-500 py-8 text-5xl ">
-    shree Ganesh 
+   My Project
    </div>
   );
 }
